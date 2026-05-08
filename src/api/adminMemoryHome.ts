9f244d693c55091becb8aca32b89f0eebba9e353
YUXIN ZHANG <yuxin2247@gmail.com>
2026-05-08 13:49:22 +0800

    Neutralize obsolete admin memory wrapper

--- src/api/adminMemoryHome.ts
+++ src/api/adminMemoryHome.ts
@@ -1,11 +1,1 @@
-import { handleAdminMemories } from "./adminMemories";
-import type { Env } from "../types";
-
-export async function handleAdminMemoryHome(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-  const response = await handleAdminMemories(request, env, ctx);
-  if (response.status !== 401) return response;
-
-  const headers = new Headers(response.headers);
-  headers.set("www-authenticate", 'Basic realm="Memory Home"');
-  return new Response(response.body, { status: response.status, statusText: response.statusText, headers });
-}
+export { handleAdminBoard as handleAdminMemoryHome } from "./adminBoard";
